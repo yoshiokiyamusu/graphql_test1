6d699acb4d501347009efb6d57fdf8fb2b379979
Yoshio <yoshiokiyamusu@gmail.com>
2021-04-10 13:00:23 -0500

    commit comment

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
+require("dotenv").config();
 import express from 'express';
 const graphqlHTTP  = require('express-graphql').graphqlHTTP /*Para integrar graphQL con express*/
 import schema from "./schema";
 import { connect } from "./database";
 
+
+
 // Initializations
 const app = express();
 connect(); //Llamar a la funcion que conecta a la BD
+
+// Settings
+app.set("port", process.env.PORT || 1200);
 
 //ruta url para renderizar 
 app.get('/',(req,res)=>{
@@ -23,4 +29,8 @@ app.use('/graphql', graphqlHTTP({
     schema:schema
 }))
 
-app.listen(1200, () => console.log('Server Running on port 1200'))
+//app.listen(1200, () => console.log('Server Running on port 1200'))
+// Starting the server
+app.listen(app.get("port"), () => {
+    console.log(`Server on port ${app.get("port")}`);
+  });
